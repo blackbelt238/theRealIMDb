bd34f6a142963b1c3f16ea75f6dac48d2aa661e3
roboemily <31740786+roboemily@users.noreply.github.com>
2017-12-04 16:03:48 -0700

    add connection string

--- sql.java
+++ sql.java
@@ -1,15 +1,13 @@
-//  Query1.java:  Query an mSQL database using JDBC.
-
 import java.sql.*;
 
-/**
- * A JDBC SELECT (JDBC query) example program.
+/*
+ * Emily Felde, Ethan Peterson, Taylor Koth
  */
 public class sql {
 
     public static void main (String[] args) {
         try {
-            String url = "jdbc:msql://200.210.220.1:1114/Demo";
+            String url = "jdbc:msql://localhost:3306/Demo";
             Connection conn = DriverManager.getConnection(url,"","");
             Statement stmt = conn.createStatement();
             ResultSet q1;
@@ -22,7 +20,6 @@ public class sql {
             while ( q1.next() ) {
                 String gross = q1.getString("gross");
                 String budget = q1.getString("budget");
-                System.out.println(lastName);
             }
             q2 = stmt.executeQuery("SELECT T.tconst, T.gross, T.budget, G.genre FROM TITLE T, GENRE G WHERE T.tconst = G.tconst");
             while ( q2.next() ) {
@@ -30,7 +27,6 @@ public class sql {
                 String gross = q2.getString("T.gross");
                 String budget = q2.getString("T.budget");
                 String genre = q2.getString("G.genre");
-                System.out.println(lastName);
             }
             q3 = stmt.executeQuery("SELECT T.tconst, T.gross, T.budget, G.genre FROM TITLE T, GENRE G WHERE T.title LIKE “Warcraft” AND T.startYear = 2016 AND T.tconst = G.tconst");
             while ( q3.next() ) {
@@ -38,21 +34,18 @@ public class sql {
                 String gross = q3.getString("T.gross");
                 String budget = q3.getString("T.budget");
                 String genre = q3.getString("G.genre");
-                System.out.println(lastName);
             }
             q4 = stmt.executeQuery("SELECT T.gross, T.language, G.genre FROM TITLE T, GENRE G WHERE T.tconst = G.tconst AND G.genre = genre");
             while ( q4.next() ) {
                 String gross = q4.getString("T.gross");
                 String language = q4.getString("T.language");
                 String genre = q4.getString("G.genre");
-                System.out.println(lastName);
             }
             q5 = stmt.executeQuery("SELECT AR.averageRating, T.gross, COUNT(T.primaryTitle) FROM TITLE T, AVERAGE_RATING AR, IS_RATED IR WHERE T.primaryTitle LIKE title AND T.tconst = IR.tconst AND IR.rconst = AR.rconst");
             while ( q5.next() ) {
                 String aveRating = q5.getString("AR.averageRating");
                 String gross = q5.getString("T.gross");
                 String genre = q5.getString("COUNT(T.primaryTitle)");
-                System.out.println(lastName);
             }
             conn.close();
         } catch (Exception e) {
