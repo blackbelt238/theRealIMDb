ac2b3e5a2a79de56a91faa617a6e61631f24b23a
roboemily <31740786+roboemily@users.noreply.github.com>
2017-12-04 22:52:50 -0700

    update queries

--- sql.java
+++ sql.java
@@ -35,9 +35,11 @@ public class sql {
             //to compare average ratings against the adult rating of titles
             q1 = stmt.executeQuery("SELECT T.isAdult, AR.averageRating FROM TITLE T, AVERAGE_RATING AR, IS_RATED IR WHERE T.tconst = IR.tconst AND IR.rconst = AR.rconst");
             while ( q1.next() ) {
+                //add boolean isAdult
                 String adult = q1.getString("T.isAdult");
                 saveStr1.append(adult);
                 saveStr1.append(",");
+                //add average rating
                 String averating = q1.getString("AR.averageRating");
                 saveStr1.append(averating);
                 saveStr1.append("\n");
@@ -46,24 +48,30 @@ public class sql {
             //to compare average rating and adult rating against the number of women in titles
             q2 = stmt.executeQuery("SELECT T.tconst, T.isAdult, AR.averageRating, G.genre, T.startYear FROM TITLE T, AVERAGE_RATING AR, IS_RATED IR, GENRE G WHERE T.tconst = IR.tconst AND IR.rconst = AR.rconst AND T.tconst = G.tconst");
             while ( q2.next() ) {
+                //add title's unique identifier and save to use in inner query
                 String q2tconst = q2.getString("T.tconst");
                 saveStr2.append(q2tconst);
                 saveStr2.append(",");
+                //add boolean isAdult
                 String adult = q2.getString("T.isAdult");
                 saveStr2.append(adult);
                 saveStr2.append(",");
+                //add average rating
                 String averating = q2.getString("AR.averageRating");
                 saveStr2.append(averating);
                 saveStr2.append(",");
+                //add genre
                 String genre = q2.getString("G.genre");
                 saveStr2.append(genre);
                 saveStr2.append(",");
+                //add title's start year
                 String year = q2.getString("T.startYear");
                 saveStr2.append(year);
                 saveStr2.append(",");
                 //secondary query to find number of women in the title
-                q2part2 = stmt.executeQuery("SELECT COUNT(PC.nconst) FROM ACTS_IN AI, PRINCIPAL_CAST PC, PRIMARY_PROFESSION PP WHERE (PP.profession LIKE “_ctress” OR PP.profession LIKE “ACTRESS”) AND AI.tconst = " + q2tconst + " AND PC.pconst = AI.pconst AND PP.nconst = PC.nconst");
+                q2part2 = stmt.executeQuery("SELECT COUNT(AI.nconst) FROM ACTS_IN AI, PRIMARY_PROFESSION PP WHERE (PP.profession LIKE “_ctress” OR PP.profession LIKE “ACTRESS”) AND AI.tconst = " + q2tconst + " AND PP.nconst = AI.nconst");
                 while ( q2part2.next() ) {
+                    //add number of actresses for each title
                     String numwomen = q2part2.getString("COUNT(PC.nconst)");
                     saveStr2.append(numwomen);
                 }
@@ -73,12 +81,15 @@ public class sql {
             //to compare average rating and for Warcraft to make predictions given the average ratings for titles in the same genre as Warcraft
             q3 = stmt.executeQuery("SELECT T.tconst, AR.averageRating, G.genre FROM TITLE T, AVERAGE_RATING AR, IS_RATED IR, GENRE G WHERE T.title LIKE “Warcraft” AND T.startYear = 2016 AND T.tconst = G.tconst AND T.tconst = IR.tconst AND IR.rconst = AR.rconst");
             while ( q3.next() ) {
+                //add title's unique identifier
                 String tconst = q3.getString("T.tconst");
                 saveStr3.append(tconst);
                 saveStr3.append(",");
+                //add average rating
                 String rating = q3.getString("AR.averageRating");
                 saveStr3.append(rating);
                 saveStr3.append(",");
+                //add genre
                 String genre = q3.getString("G.genre");
                 saveStr3.append(genre);
                 saveStr3.append("\n");
@@ -87,23 +98,28 @@ public class sql {
             //to compare average rating and language for a specific genre
             q4 = stmt.executeQuery("SELECT AR.averageRating, T.language, G.genre FROM TITLE T, AVERAGE_RATING AR, IS_RATED IR, GENRE G WHERE T.tconst = IR.tconst AND IR.rconst = AR.rconst AND T.tconst = G.tconst AND G.genre = " + genreString);
             while ( q4.next() ) {
+                //add average rating
                 String rating = q4.getString("AR.averageRating");
                 saveStr4.append(rating);
                 saveStr4.append(",");
+                //add title's original language
                 String language = q4.getString("T.language");
                 saveStr4.append(language);
                 saveStr4.append(",");
+                //add genre
                 String genre = q4.getString("G.genre");
                 saveStr4.append(genre);
                 saveStr4.append("\n");
             }
             //query five
             //to compare average rating and number of titles for the star wars and star trek franchises
-            q5 = stmt.executeQuery("SELECT AR.averageRating, (T.primaryTitle LIKE '%_tar _ars%')+1 FROM TITLE T, AVERAGE_RATING AR, IS_RATED IR WHERE T.primaryTitle LIKE '%_tar _ars%' OR T.primaryTitle LIKE '%_tar _rek%'AND T.tconst = IR.tconst AND IR.rconst = AR.rconst");
+            q5 = stmt.executeQuery("SELECT AR.averageRating, (T.primaryTitle LIKE '%_tar _ars%')+1 FROM TITLE T, AVERAGE_RATING AR, IS_RATED IR WHERE T.primaryTitle LIKE '%_tar _ars%' OR T.primaryTitle LIKE '%_tar _rek%' AND T.tconst = IR.tconst AND IR.rconst = AR.rconst");
             while ( q5.next() ) {
+                //add average rating
                 String aveRating = q5.getString("AR.averageRating");
                 saveStr5.append(aveRating);
                 saveStr5.append(",");
+                //add primary title
                 String count = q5.getString("COUNT(T.primaryTitle)");
                 saveStr5.append(count);
                 saveStr5.append("\n");
@@ -116,18 +132,23 @@ public class sql {
             FileWriter filewrite3 = null;
             FileWriter filewrite4 = null;
             FileWriter filewrite5 = null;
-            //try writing to files
+            //try writing to .csv files
             try{
               filewrite1 = new FileWriter("q1.csv");
               filewrite1.append(saveStr1.toString());
+              System.out.println("Generated q1.csv");
               filewrite2 = new FileWriter("q2.csv");
               filewrite2.append(saveStr2.toString());
+              System.out.println("Generated q2.csv");
               filewrite3 = new FileWriter("q3.csv");
               filewrite3.append(saveStr3.toString());
+              System.out.println("Generated q3.csv");
               filewrite4 = new FileWriter("q4.csv");
               filewrite4.append(saveStr4.toString());
+              System.out.println("Generated q4.csv");
               filewrite5 = new FileWriter("q5.csv");
               filewrite5.append(saveStr5.toString());
+              System.out.println("Generated q5.csv");
             }
             catch(Exception e){
               System.out.println(e);
